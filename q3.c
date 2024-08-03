ce5cf034a4e5afe3cddc5bbf1da80b3eca234a03
Nitinyadav-code <168860833+Nitinyadav-code@users.noreply.github.com>
2024-08-03 12:55:42 +0530

    Create q3.c

---
+++ q3.c
@@ -0,0 +1,12 @@
+//question3
+
+/*
+Character Frequency Swap
+Question: Write a C program that reads a string from the user and swaps the most frequent character with the least frequent character in the string.
+
+Description: The program should take a string input from the user, determine the most and least frequent characters (case insensitive), and swap their occurrences in the string.
+Sample Input:
+abracadabra
+Sample Output:
+cbrcacdcbrc
+*/
